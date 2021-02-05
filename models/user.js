84706ea14782c44153bf69611c0dcff905e6a75a
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2021-02-05 10:17:01 +0900

    Update user.js

--- models/user.js
+++ models/user.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      user.hasMany(models.voice, {
+        foreignKey: 'userId',
+      });
     }
   }
   user.init(
