bdd323f196b3a042c68c861a12eb754d4fe09092
code-kay <kay8563@gmail.com>
2021-02-05 16:54:45 +0900

    #56 controllers/voice_board(POST)

---
+++ controllers/voice_board.js
@@ -0,0 +1,15 @@
+// var Voice_Board = require('../models').voice_board;
+const {models} = require('../models');
+const Voice_Board = models.voice_board
+
+module.exports = {
+    post : function (req,res) {
+        Voice_Board.create({
+            boardId: req.body.boardId,
+            voiceId: req.body.voiceId
+        })
+        .then(voice_board => {
+            res.send({id: voice_board.id})
+        })
+    }
+}
