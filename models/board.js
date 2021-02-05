83a946694e17490d750b78ea9b666143ea2a23e3
code-kay <73449966+code-kay@users.noreply.github.com>
2021-02-05 10:55:03 +0900

    #41 models/board
    models/board

---
+++ models/board.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class board extends Model {
+    static associate(models) {
+      board.belongsToMany(models.voice, {
+        through: 'voice_board',
+      });
+    }
+  }
+  board.init(
+    {
+      title: DataTypes.STRING,
+      like_count: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'board',
+    }
+  );
+  return board;
+};
