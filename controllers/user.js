9eac4f92ee0b535aa9a4d015694b0b8a1f4c8ed8
SungSeokMin <jkl154527@gmail.com>
2021-02-06 14:11:19 +0900

    Complete user_logout

--- controllers/user.js
+++ controllers/user.js
@@ -68,10 +68,41 @@ module.exports = {
     },
   },
   login: {
-    post: (req, res) => {},
+    post: (req, res) => {
+      const { email, password } = req.body;
+      user.findOne({ where: { email, password } }).then((result) => {
+        if (result === null) {
+          res.status(404).send({ message: 'invalid user' });
+        } else {
+          const { id } = result.dataValues;
+          req.session.userId = id;
+          res.status(200).send({ id });
+        }
+      });
+    },
   },
   signout: {
-    post: {},
+    post: (req, res) => {
+      // TODO: client에서 넘어온 sessionId를 가지고 DB에서 유저 검색
+      // TODO: 찾은 user의 is_signed_up의 값이 true이면 일반 유저, false면 비회원 로그인
+      // TODO: 일반 유저는 session만 삭제, 비회원 로그인은 DB에서 정보를 삭제후 session 삭제
+      const { userId } = req.session;
+      user.findOne({ where: { id: userId } }).then((result) => {
+        const { id, is_signed_up } = result;
+        // 일반 유저의 로그아웃 요청
+        if (is_signed_up) {
+          req.session.destroy();
+          res.status(205).send({ message: 'logout success' });
+        } else {
+          // 비회원 유저의 로그아웃 요청
+          //* session을 삭제한 후 DB에서 user의 정보 삭제
+          req.session.destroy();
+          user.destroy({ where: { id } }).then(() => {
+            res.status(205).send({ message: 'tempUser logout success' });
+          });
+        }
+      });
+    },
   },
   voicelist: {
     get: {},
