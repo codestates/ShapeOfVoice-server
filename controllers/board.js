b69377119bc1c014f84519ddbf6dda775da90b18
code-kay <kay8563@gmail.com>
2021-02-08 15:37:55 +0900

    #54 added user authentification

--- controllers/board.js
+++ controllers/board.js
@@ -34,13 +34,21 @@ module.exports = {
   },
 
   delete: function (req, res) {
-    voice_board.findOne({
-      where : {boardId : req.body.id}
-    }).then(voice_board => {
-      voice.destroy({where: {id : voice_board.voiceId}})
-      board.destroy({where: {id: req.body.id}})
-    }).then(()=>res.send())
-    .catch(err => res.send(err))
+    board.findOnd({
+      where : {id : req.body.id}
+    }).then(board =>{
+      if (board.userId !== req.session.userId){
+        res.status(404).send({message: "invalid user"})
+      }
+    }).then(()=>{
+      voice_board.findOne({
+        where : {boardId : req.body.id}
+      }).then(voice_board => {
+        voice.destroy({where: {id : voice_board.voiceId}})
+        board.destroy({where: {id: req.body.id}})
+      }).then(()=>res.send())
+      .catch(err => res.send(err))
+    })
   },
 
   detail: {
