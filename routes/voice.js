f33ecf0b1c70cad3987a62bee1fd60c2e1155dc7
code-kay <73449966+code-kay@users.noreply.github.com>
2021-02-08 22:42:15 +0900

    Update voice.js

--- routes/voice.js
+++ routes/voice.js
@@ -1,8 +1,7 @@
 const express = require('express');
-// const router = express.Router();
+const router = express.Router();
 
-// controller생성 후 작성
-// const { voiceController } = require('../controller/')
+const voiceController = require('../controller/voice')
+router.post('/', voiceCon.post);
 
-// voiceCreate
-// router.post('/', voiceCon.post);
+module.exports = router;
