bb73cbf754ca68d533a022064ea4dcdab4691497
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2021-02-08 11:49:59 +0900

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -109,7 +109,43 @@ module.exports = {
     },
   },
   voicelist: {
-    get: async (req, res) => {},
+    get: (req, res) => {
+      // TODO: mypage에서 보여지는 정보를 찾아야 한다.(Thumbnail, Board Title, Board created At, nickname, e-mail)
+      // TODO: user <-> voice <-> voice_board <-> board
+      // TODO: ni,em    userId    voID, boId      id
+      const { userId } = req.session;
+
+      // 유저의 Thumbnail, Board Title, Board created At, nickname, e-mail 찾기
+      user
+        .findOne({
+          attributes: ['nickname', 'email'],
+          where: { id: userId },
+          include: [
+            {
+              model: voice,
+              attributes: ['thumbnail'],
+              include: [
+                {
+                  model: board,
+                  attributes: ['title', 'createdAt'],
+                  through: { attributes: [] },
+                },
+              ],
+            },
+          ],
+        })
+        .then((result) => {
+          // const data = {};
+          // data.nickname = result.nickname;
+          // data.email = result.email;
+          // data.thumbnail = result.voices.map((el) => el.thumbnail);
+          // data.boards = result.voices.map((el) => el.boards).flat(2);
+          res.status(200).send({ data: result });
+        })
+        .catch((err) => {
+          res.send(err);
+        });
+    },
   },
   get: (req, res) => {
     // TODO: client에서 렌더 되기 전에 호출을 당해 해당 유저의 정보를 넘겨준다.
