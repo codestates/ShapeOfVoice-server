8ccae75f6deb8ad75c2a316eb209d8486dd92331
code-kay <kay8563@gmail.com>
2021-02-08 15:19:15 +0900

    #35 board/detail method changed from get to post

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,11 @@
-// const boardCon = require('../controllers/board');
+const boardCon = require('../controllers/board');
 const express = require('express');
 const router = express.Router();
 
-// router.post('/', boardCon.post)
-// router.patch('/', boardCon.patch)
-// router.delete('/', boardCon.delete)
-// router.get('/detail', boardCon.detail.get)
-// router.get('/list', boardCon.list.get)
+router.post('/', boardCon.post)
+router.patch('/', boardCon.patch)
+router.delete('/', boardCon.delete)
+router.get('/detail', boardCon.detail.post)
+router.get('/list', boardCon.list.get)
 
 module.exports = router;
