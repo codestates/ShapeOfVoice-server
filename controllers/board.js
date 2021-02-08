bf1bc0d0c9dbcaa71b0cb6e53839269f26af2d83
SungSeokMin <jkl154527@gmail.com>
2021-02-08 11:13:31 +0900

    Complete boardList

--- controllers/board.js
+++ controllers/board.js
@@ -1,34 +1,50 @@
 var Board = require('../models').board;
+const Board = require('../models').board;
+const User = require('../models').user;
+const Voice = require('../models').voice;
 
 module.exports = {
-    post : function (req,res) {
-        Board.create({ title: req.body.title })
-        .then(board => {
-            console.log(board)
-            console.log(board.id)
-            res.send({id: board.id})
+  post: function (req, res) {
+    Board.create({ title: req.body.title }).then((board) => {
+      console.log(board);
+      console.log(board.id);
+      res.send({ id: board.id });
+    });
+  },
+
+  patch: function (req, res) {},
+
+  delete: function (req, res) {},
+
+  detail: {
+    get: function (req, res) {},
+  },
+
+  list: {
+    get: function (req, res) {
+      // TODO: 게시판의 전체 목록을 보여준다.
+      // user table = nickname, voice table = thumbnail, board table = title, createdAt
+      // 게시판의 전체 목록을
+      User.findAll({
+        attributes: ['nickname'],
+        include: [
+          {
+            model: Voice,
+            attributes: ['thumbnail'],
+            include: {
+              model: Board,
+              attributes: ['title', 'createdAt'],
+              through: { attributes: [] },
+            },
+          },
+        ],
+      })
+        .then((result) => {
+          res.status(200).send(result);
         })
+        .catch((err) => {
+          res.send(err);
+        });
     },
-
-    patch : function (req,res) {
-
-    },
-
-    delete : function (req,res) {
-
-    },
-
-    detail: {
-
-        get: function (req,res) {
-
-        }
-    },
-
-    list: {
-
-        get: function (req,res) {
-
-        }
-    }
-}
+  },
+};
