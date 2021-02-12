4a1501ef47c1fbdd1e673ecda256dbf229ae9347
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2021-02-12 12:35:04 +0900

    Update voice.js

--- controllers/voice.js
+++ controllers/voice.js
@@ -1,14 +1,14 @@
-var Voice = require('../models').voice;
+const { voice } = require('../models');
 
 module.exports = {
-    post : function (req,res) {
-        Voice.create({
-            thumbnail: req.body.thumbnail,
-            records: req.body.records,
-            userId: req.session.userId
-        })
-        .then(voice => {
-            res.send({id: voice.id})
-        })
-    }
-}
+  post: function (req, res) {
+    voice
+      .create({
+        thumbnail: req.body.thumbnail,
+        userId: req.session.userId,
+      })
+      .then((voice) => {
+        res.send({ id: voice.id });
+      });
+  },
+};
