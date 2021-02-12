92e8544bb1a44a33b548aa5a81dea2a74f382f0e
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2021-02-12 12:40:23 +0900

    add incrementLikeCount, decrementLikeCount

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,13 @@
-const boardCon = require('../controllers/board');
 const express = require('express');
 const router = express.Router();
+const boardCon = require('../controllers/board');
 
-router.post('/', boardCon.post)
-router.patch('/', boardCon.patch)
-router.delete('/', boardCon.delete)
-router.get('/detail', boardCon.detail.post)
-router.get('/list', boardCon.list.get)
+router.post('/', boardCon.post);
+router.put('/incrementLikeCount', boardCon.incrementLikeCount.put);
+router.put('/decrementLikeCount', boardCon.decrementLikeCount.put);
+router.patch('/', boardCon.patch);
+router.delete('/', boardCon.delete);
+router.post('/detail', boardCon.detail.post);
+router.get('/list', boardCon.list.get);
 
 module.exports = router;
