85a0a45bb260afef14dd92014f7674456f10e9f3
SungSeokMin <72539723+SungSeokMin@users.noreply.github.com>
2021-02-12 12:33:06 +0900

    update increment, decrement

--- controllers/board.js
+++ controllers/board.js
@@ -1,15 +1,15 @@
-const { board, user, voice, voice_board, Sequelize } = require ('../models')
+const { board, user, voice, voice_board, Sequelize } = require('../models');
 
 module.exports = {
   post: function (req, res) {
-    board.create({
-      title: req.body.title,
-      userId: req.session.userId
-    }).then((board) => {
-      console.log(board);
-      console.log(board.id);
-      res.send({ id: board.id });
-    });
+    board
+      .create({
+        title: req.body.title,
+        userId: req.session.userId,
+      })
+      .then((board) => {
+        res.send({ id: board.id });
+      });
   },
 
   patch: function (req, res) {
@@ -23,57 +23,94 @@ module.exports = {
     // title을 바꾸길 원하는 user의 sessionId 가 있으면 로직 수행
     if (userId) {
       // board table의 userId = sessionId, id = req.body.id(해당 게시물 번호)
-      board.update(
-        { title, updatedAt: Sequelize.DATE },
-        { where: { userId, id } }
-      ).then(() => {
-        res.send({ message: 'update success' });
-      });
+      board
+        .update({ title, updatedAt: Sequelize.DATE }, { where: { userId, id } })
+        .then(() => {
+          res.send({ message: 'update success' });
+        });
     }
   },
 
+  incrementLikeCount: {
+    put: function (req, res) {
+      // client에서 board의 id를 받아온다.
+      const { id } = req.body;
+      // 기존 likecount에서 1씩 증가
+      // 받아온 id로 board테이블에서 글을 조회
+      board
+        .findOne({ where: { id } })
+        .then((result) => {
+          return result.increment('like_count', { by: 1 });
+        })
+        .then(() => {
+          res.status(201).send({ message: 'Increase complete' });
+        });
+    },
+  },
+
+  decrementLikeCount: {
+    put: function (req, res) {
+      // 기존 likecount에서 1씩 감소
+      const { id } = req.body;
+      // 기존 likecount에서 1씩 증가
+      // 받아온 id로 board테이블에서 글을 조회
+      board
+        .findOne({ where: { id } })
+        .then((result) => {
+          return result.decrement('like_count', { by: 1 });
+        })
+        .then(() => {
+          res.status(201).send({ message: 'Decrease complete' });
+        });
+    },
+  },
 
   delete: function (req, res) {
-    board.findOnd({
-      where : {id : req.body.id}
-    }).then(board =>{
-      if (board.userId !== req.session.userId){
-        res.status(404).send({message: "invalid user"})
-      }
-    }).then(()=>{
-      voice_board.findOne({
-        where : {boardId : req.body.id}
-      }).then(voice_board => {
-        voice.destroy({where: {id : voice_board.voiceId}})
-        board.destroy({where: {id: req.body.id}})
-      }).then(()=>res.send())
-      .catch(err => res.send(err))
-    })
+    board
+      .findOne({
+        where: { id: req.body.id },
+      })
+      .then((board) => {
+        if (board.userId !== req.session.userId) {
+          res.status(404).send({ message: 'invalid user' });
+        }
+      })
+      .then(() => {
+        voice_board
+          .findOne({
+            where: { boardId: req.body.id },
+          })
+          .then((voice_board) => {
+            voice.destroy({ where: { id: voice_board.voiceId } });
+            board.destroy({ where: { id: req.body.id } });
+          })
+          .then(() => res.send())
+          .catch((err) => res.send(err));
+      });
   },
 
   detail: {
     post: function (req, res) {
-      console.log(req.body.id)
-      board.findOne({
-        attributes: ['title', 'like_count', 'createdAt'],
-        where: {id: req.body.id},
-        include: [
-          {
-            model: user,
-            attributes: ['nickname']
-          },
-          {
-            model: voice,
-            attributes: ['records'],
-            through: { attributes: [] }
-          }
-        ]
-      })
-      .then(result => {
-        console.log(result)
-        res.send({result: result})
-      })
-      .catch(err => res.send(err))
+      board
+        .findOne({
+          attributes: ['title', 'like_count', 'createdAt'],
+          where: { id: req.body.id },
+          include: [
+            {
+              model: user,
+              attributes: ['nickname'],
+            },
+            {
+              model: voice,
+              attributes: ['thumbnail'],
+              through: { attributes: [] },
+            },
+          ],
+        })
+        .then((result) => {
+          res.send({ result });
+        })
+        .catch((err) => res.send(err));
     },
   },
 
@@ -82,22 +119,23 @@ module.exports = {
       // TODO: 게시판의 전체 목록을 보여준다.
       // user table = nickname, voice table = thumbnail, board table = title, createdAt
       // 게시판의 전체 목록을
-      user.findAll({
-        attributes: ['nickname'],
-        include: [
-          {
-            model: voice,
-            attributes: ['thumbnail'],
-            include: {
-              model: board,
-              attributes: ['id', 'title', 'createdAt'],
-              through: { attributes: [] },
+      user
+        .findAll({
+          attributes: ['nickname'],
+          include: [
+            {
+              model: voice,
+              attributes: ['thumbnail'],
+              include: {
+                model: board,
+                attributes: ['id', 'title', 'createdAt'],
+                through: { attributes: [] },
+              },
             },
-          },
-        ],
-      })
+          ],
+        })
         .then((result) => {
-          res.status(200).send(result);
+          res.status(200).send({ result });
         })
         .catch((err) => {
           res.send(err);
